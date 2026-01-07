3af3645b7f7296bcb3750eab28d99260462daca7
raphael-as <128089668+raphael-as@users.noreply.github.com>
2026-01-07 16:40:37 +0200

    Test

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,5 +1,5 @@
 export const contactsData = {
-    email: 'raphael@abi-saad.fr',
+    email: 'raphael@abi-saad.frR',
     phone: '+961 76 544 842',
     address: 'Lebanon 🇱🇧',
     webhookURL: 'https://discord.com/api/webhooks/1457036466218864652/3lx9d7UldESs97cquNQCLVO6TEz1d-LI9vw0YRv4TA0jdrhGjra5Oyj4Bm_9IrPQbqEE'
